1a5c3ab04fe02258a7266e6d69bdc6214d2acbe6
ArmandoEnrique1010K <81869631+enrique1010k@users.noreply.github.com>
2023-07-24 00:02:30 -0500

    CRUD - IMAGENES
    CRUD para subir imagenes con SpringBoot y JavaScript. ChatGPT ayuda en el codigo fuente.

--- InitializrSpringbootProject/src/main/java/com/prueba01/repository/GaleriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prueba01.repository;
-
-import com.prueba01.entity.GaleriaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GaleriaRepository extends JpaRepository<GaleriaEntity, Long>{
-    
-}
