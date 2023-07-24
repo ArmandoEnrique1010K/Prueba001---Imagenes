b2ebf110b9db18ac511330bd7b97260a8ebd5d4d
ArmandoEnrique1010K <81869631+enrique1010k@users.noreply.github.com>
2023-07-24 00:03:08 -0500

    CRUD - IMAGENES
    CRUD para subir imagenes con SpringBoot y JavaScript.

---
+++ InitializrSpringbootProject/src/main/java/com/prueba01/service/ContenedorImagenes.java
@@ -0,0 +1,19 @@
+package com.prueba01.service;
+
+import java.nio.file.Path;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ContenedorImagenes {
+
+    public void iniciarContenedorImagenes();
+
+    public String almacenarUnaImagen(MultipartFile imagen);
+
+    public Path cargarImagen(String nombreImagen);
+
+    public Resource cargarComoRecurso(String nombreImagen);
+
+    public void eliminarImagen(String nombreImagen);
+    
+}
---
+++ InitializrSpringbootProject/src/test/java/com/prueba01/CrudImagenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudImagenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
